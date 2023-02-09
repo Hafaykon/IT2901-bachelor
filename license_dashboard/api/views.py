cc9f80a09b268328c8134a9d78e1977c44ba5f28
hakon <haakonwu@stud.ntnu.no>
2023-02-09 14:07:38 +0100

    Added a view function to test Pipeline

--- license_dashboard/api/views.py
+++ license_dashboard/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import pandas as p
 
 
 # Create your views here.
@@ -8,3 +9,10 @@ def get_routes(request):
     routes = [
     ]
     return Response('The api is working')
+
+
+@api_view(['GET'])
+def get_dataframe(request):
+    df = p.read_csv('./data/compliance_per_computer_bertil.csv')
+    df = df.fillna('-')
+    return Response(df.columns)
