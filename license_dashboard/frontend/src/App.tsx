7864fa4f7e98f500a73246e66af4c7540ee270d0
Alvaro W <alvarow@stud.ntnu.no>
2023-02-09 15:14:37 +0100

    Revert "Merge branch 'test-pipeline' into '12-ci-cd-pipeline'"
    This reverts merge request !6

--- license_dashboard/frontend/src/App.tsx
+++ license_dashboard/frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@mui/material';
 import React from 'react';
 import './App.css';
 
@@ -7,13 +6,7 @@ function App() {
     <div className={'App'}>
       <h1>Hello</h1>
     </div>
-    <div className={'App'}>
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
-        <Button variant="outlined">Outlined</Button>
-    </div>
   );
 }
-
 
 export default App;
