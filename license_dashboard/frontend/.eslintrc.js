2db7294686e8936f4852bd714501385a9e935593
hakon <haakonwu@stud.ntnu.no>
2023-02-09 12:58:08 +0100

    Added required settings for rest_framework and eslint. Also added a '/' in url's.

--- license_dashboard/frontend/.eslintrc.js
+++ license_dashboard/frontend/.eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 0
   }
 };
