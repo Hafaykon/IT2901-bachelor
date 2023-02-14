618da9a2e897562ddaf7d394a5b6c2d3b939ea0c
ida høyland <idawh@stud.ntnu.no>
2023-02-14 13:15:20 +0100

    #4: implemented a rough draft of the statistic component. Co-authored by: @solveimy

--- dashboard/frontend/src/App.tsx
+++ dashboard/frontend/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import './App.css';
 import InfoBox from './components/dashboard/InfoBox';
-import { Stack } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Stack, Typography } from '@mui/material';
+import { PieChart } from 'react-minimal-pie-chart';
+
+
+const defaultLabelStyle = {
+  fontSize: '5px',
+  fontFamily: 'sans-serif',
+};
 
 function App() {
   return (
@@ -11,10 +18,36 @@ function App() {
         <InfoBox title="Aktive Lisenser" numberOfLicenses={2000} />
         <InfoBox title="Allokerbare Lisenser" numberOfLicenses={100} />
       </Stack>
-    </div></>
+    </div><div>
+    <Card id="stats">
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary">
+          Overskrift
+        </Typography>
+        <PieChart
+          viewBoxSize={[140,140]}
+          center={[70,40]}
+          radius={30}
+          label={({ dataEntry }) => dataEntry.key}
+          labelStyle={{
+            ...defaultLabelStyle,
+          }}
+          data={[
+                { title: 'One', value: 20, color: '#E38627', key:"20%" },
+                { title: 'Two', value: 30, color: '#C13C37', key:"30%" },
+                { title: 'Three', value: 50, color: '#6A2135', key:"50%" },
+                
+          ]}
+          
+        />;
+      </CardContent>
+    </Card></div></>
    
   );
 }
+
+
+
 
 
 export default App;
