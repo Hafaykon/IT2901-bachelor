2662697b9cb2f23e1b0555092c97354c10fa08be
ida høyland <idawh@stud.ntnu.no>
2023-02-16 15:01:10 +0100

    #2: added chart template and card container. Co-authored by: @solveimy

---
+++ dashboard/frontend/src/components/dashboard/DonutChart.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Button, Card, CardActions, CardContent, Stack, Typography } from '@mui/material';
+import { PieChart } from 'react-minimal-pie-chart';
+
+const defaultLabelStyle = {
+  fontSize: '5px',
+  fontFamily: 'sans-serif',
+};
+
+function DonutChart(){
+
+  return(
+  <div>
+    <Card sx={{height:400,width:663,mt: 7, ml:14}}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary">
+          Overskrift
+        </Typography>
+        <PieChart
+          viewBoxSize={[140,140]}
+          center={[70,40]}
+          radius={30}
+          label={({ dataEntry }) => dataEntry.key}
+          labelStyle={{
+            ...defaultLabelStyle,
+          }}
+          data={[
+                { title: 'One', value: 20, color: '#E38627', key:"20%" },
+                { title: 'Two', value: 30, color: '#C13C37', key:"30%" },
+                { title: 'Three', value: 50, color: '#6A2135', key:"50%" },
+                
+          ]}
+          
+        />;
+      </CardContent>
+    </Card></div>
+  );
+}
+
+export default DonutChart;
