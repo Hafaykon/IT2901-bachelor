451cc1d8619ea01930c63c65023bbaa49122d5de
Solveig <solveimy@stud.ntnu.no>
2023-02-21 09:58:18 +0100

    #5 Changed savingsBox to a new component and changed some css to fit both InfoBox and SavingsBox components.

--- dashboard/frontend/src/components/dashboard/SavingsBox.tsx
+++ dashboard/frontend/src/components/dashboard/SavingsBox.tsx
@@ -1,24 +1,21 @@
-
 import { Card, CardActionArea, CardContent, Typography } from '@mui/material';
 import * as React from 'react';
 
-const InfoBox = (props: {title: string, numberOfLicenses: number}) => {
+interface SavingsPrpos {
+    savings: number
+}
+
+export const SavingsBox = (props: {savings: number}) => {
 
     return (
         <Card sx={{ width: 300, height: 200}}>
-            <CardActionArea sx={{paddingTop: 2, paddingBottom: 4}}>
                 <CardContent>
                   <Typography gutterBottom component="div" id="cardTitle">
-                    {props.title}
                   </Typography>
                   <Typography color="text.secondary" id="numbersBoxes">
-                    {props.numberOfLicenses}
+                    {props.savings}
                   </Typography>
                 </CardContent>
-              </CardActionArea>
             </Card>
     )
 }
-
-export default InfoBox;
-
