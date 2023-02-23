ca0b040d936c4b186a8cde83467fdd87b03184c7
ida høyland <idawh@stud.ntnu.no>
2023-02-23 12:13:02 +0100

    #4: Fixed the styling to the statistic component. Co-authored by: @solveimy'

--- dashboard/frontend/src/App.test.tsx
+++ dashboard/frontend/src/App.test.tsx
@@ -2,6 +2,8 @@ import { act, render, screen } from '@testing-library/react';
 import App from "./App"
 import InfoBox from './components/dashboard/InfoBox';
 import '@testing-library/jest-dom/extend-expect';
+import ReactDOM from 'react-dom';
+import DonutChart from './components/dashboard/DonutChart';
 
 
 describe("Testing render, infoboxes", () => {
@@ -11,6 +13,13 @@ describe("Testing render, infoboxes", () => {
       screen.getByText(/Lisenser/)
     ).toBeInTheDocument();
   });
+});
+
+
+it("renders donutchart without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<DonutChart />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
 
 
