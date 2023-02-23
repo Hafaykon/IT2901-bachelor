8677e6502a53c0a1df6ee81fe9353c3e59413611
emmablix <emmablixx@gmail.com>
2023-02-23 13:15:05 +0100

    #1: Created test for navbvar
    Co-Authered-By: Sarmi Ponnuthurai @sarmip

--- dashboard/frontend/src/components/navbar/Navbar.test.tsx
+++ dashboard/frontend/src/components/navbar/Navbar.test.tsx
@@ -3,12 +3,7 @@ import { MemoryRouter } from 'react-router-dom';
 import Navbar from './NavBar';
 import '@testing-library/jest-dom/extend-expect';
 
-// Test that the navbar is rendered
-{/* MemoryRouter is used because 
-This error message indicates that you're trying to use the useLocation hook outside of a Router component.
-The useLocation hook is part of the react-router-dom library and can only be used within a Router component or one of its descendants.
-To fix this error, you need to make sure that your test file is wrapped in a Router component so that useLocation can be used within it.
-*/}
+// Testisng that the navbar is rendered correctly
 test('renders navbar', () => {
   render(
     <MemoryRouter>
@@ -18,3 +13,21 @@ test('renders navbar', () => {
   const navbarElement = screen.getByTestId('navbar');
   expect(navbarElement).toBeInTheDocument();
 });
+
+// Testing that the navbar contains the correct links
+test('navbar contains correct links', () => {
+    render(
+        <MemoryRouter>
+        <Navbar />
+        </MemoryRouter>
+    );
+    const dashboardLink = screen.getByText('Dashboard');
+    const myPageLink = screen.getByText('Min side');
+    const licensePoolLink = screen.getByText('Lisensportal');
+    const faqLink = screen.getByText('FAQ');
+    expect(dashboardLink).toBeInTheDocument();
+    expect(myPageLink).toBeInTheDocument();
+    expect(licensePoolLink).toBeInTheDocument();
+    expect(faqLink).toBeInTheDocument();
+    });
+
