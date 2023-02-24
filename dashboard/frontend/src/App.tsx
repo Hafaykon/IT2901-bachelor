364fe8f76c4d2725fd5bfcf86a20371ee9c6e8b6
sarmip <sarmip@stud.ntnu.no>
2023-02-24 14:55:12 +0100

    #1 Added navbar Co-Authored-By: Emma Blix @ejbjur

--- dashboard/frontend/src/App.tsx
+++ dashboard/frontend/src/App.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './App.css';
 import {RecoilRoot} from 'recoil';
-import Dashboard from "./components/dashboard/Dashboard";
 import {Route, Routes} from 'react-router-dom';
-import MyPage from "./pages/MyPage";
-import FAQPage from "./pages/FAQPage";
-import LicensePool from "./components/licensepool/LicensePool";
-import NavBar from "./components/navigation/NavBar";
+import MyPage from './pages/Mypage';
+import FAQ from './pages/FAQ';
+import Home from './pages/Home';
+import Navbar from './components/navbar/Navbar';
+import Licenses from './pages/Licenses';
+
 function App() {
+    
     return (
         <RecoilRoot>
-            <NavBar/>
+           <Navbar/> 
             <Routes>
-                <Route path="/" element={<Dashboard/>}/>
-                <Route path="/minside" element={<MyPage/>}/>
-                <Route path="/lisensportal" element={<LicensePool/>}/>
-                <Route path="/FAQ" element={<FAQPage/>}/>
+                <Route path="/" element={<Home/>}/>
+                 <Route path="/minside" element={<MyPage/>}/> 
+                <Route path="/lisensportal" element={<Licenses/>}/>
+                 <Route path="/FAQ" element={<FAQ/>}/>
             </Routes>
         </RecoilRoot>
     );
