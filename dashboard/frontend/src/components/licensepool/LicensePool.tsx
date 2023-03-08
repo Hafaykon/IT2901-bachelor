64bfd73fbcf7500db1c8ffd867e4e0ebcb284763
Vegard Henriksen <vnhenrik@stud.ntnu.no>
2023-03-08 19:46:35 +0100

    table for showing licenses

--- dashboard/frontend/src/components/licensepool/LicensePool.tsx
+++ dashboard/frontend/src/components/licensepool/LicensePool.tsx
@@ -1,36 +1,34 @@
-import { Grid, Stack } from '@mui/material';
+import {Grid, Stack} from '@mui/material';
 import React from 'react';
 import SoftwareSearchBar from '../search/SoftwareSeachBar';
-import FilterSelect from '../search/FilterSelect';
-import SortSelect from '../search/SortSelect';
+import LicenseTableV2 from "./LicenseTableV2";
 
 function LicensePool() {
-  return (
-    <div id={'licensepool_container'} style={{ display: 'flex', justifyContent: 'center' }}>
-      <Grid container className='license_pool'>
-        <Grid item>
-          <Stack direction='row' spacing={20}>
-            <h1>Lisensportalen</h1>
-          </Stack>
-        </Grid>
-        <Grid container className={'license_parameters'}
-              style={{ display: 'flex', justifyContent: 'space-evenly', marginBottom: '10px' }}>
-          <Grid item>
-            <Stack direction='row' spacing={5}>
-              <SoftwareSearchBar />
-            </Stack>
-          </Grid>
-        </Grid>
-        <br />
-        <Grid container style={{ display: 'flex', justifyContent: 'center' }} className={'license_table'}>
-          <FilterSelect />
-          <SortSelect />
-        </Grid>
+    return (
+        <div id={'licensepool_container'} style={{display: 'flex', justifyContent: 'center'}}>
+            <Grid container className='license_pool'>
+                <Grid item>
+                    <Stack direction='row' spacing={20}>
+                        <h1>Lisensportalen</h1>
+                    </Stack>
+                </Grid>
+                <Grid container className={'license_parameters'}
+                      style={{display: 'flex', justifyContent: 'space-evenly', marginBottom: '10px'}}>
+                    <Grid item>
+                        <Stack direction='row' spacing={5}>
+                            <SoftwareSearchBar show={true}/>
+                        </Stack>
+                    </Grid>
+                </Grid>
+                <br/>
+                <Grid container style={{display: 'flex', justifyContent: 'center', alignItems: "center", width: "100%"}} className={'license_table'}>
+                    <LicenseTableV2/>
+                </Grid>
 
 
-      </Grid>
-    </div>
-  );
+            </Grid>
+        </div>
+    );
 }
 
 export default LicensePool;
