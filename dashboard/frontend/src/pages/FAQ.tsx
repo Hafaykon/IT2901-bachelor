06fc74167801aa75e63879bad4033f2a82f4d850
Solveig <solveimy@stud.ntnu.no>
2023-03-29 12:59:56 +0200

    Created columns based on themes and plan on adding accordion buttons in each column

--- dashboard/frontend/src/pages/FAQ.tsx
+++ dashboard/frontend/src/pages/FAQ.tsx
@@ -6,11 +6,28 @@ import './FAQ.css';
 function FAQ() {
     return (
         <div>
-            <Grid sx={{paddingTop: 5, paddingLeft: 25}}>
+            <Grid container sx={{paddingTop: 5, paddingLeft: 25}}>
                 <ActiveLastBreadcrumb />
             </Grid>
-            <Grid className='Container'>
+            <Grid className='Header'>
                 <Typography id="Headline">Ofte stilte spørsmål</Typography>
+                <p style={{ fontStyle: 'italic', marginBottom: '30px' }}>
+                    Nedenfor ser du en oversikt over ofte stilte spørsmål om lisensdashboardet, 
+                    lisensportalen, og lisensadministrering generelt. 
+                </p>
+            </Grid>
+            <Grid container id="Columns">
+                <Grid container rowSpacing={2} id="FAQs">
+                    <Grid item xs={4}>
+                        <h3 className='SubHeadline'>Lisensdashboard</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3 className='SubHeadline'>Lisensportalen</h3>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <h3 className='SubHeadline'>Lisensadministrering generelt</h3>
+                    </Grid>
+                </Grid>
             </Grid>
         </div>
     );
