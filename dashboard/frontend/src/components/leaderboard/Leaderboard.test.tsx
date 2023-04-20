ce8c3bb8eb0800c2fd11b0d3a4d17b771038bf08
sarmip <sarmip@stud.ntnu.no>
2023-04-20 21:09:48 +0200

    #65 Created test co-authored by: @idawh

--- dashboard/frontend/src/components/leaderboard/Leaderboard.test.tsx
+++ dashboard/frontend/src/components/leaderboard/Leaderboard.test.tsx
@@ -1,22 +1,65 @@
-import {cleanup, render, screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { Leaderboard } from './Leaderboard';
+import { MemoryRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import '@testing-library/jest-dom/extend-expect';
-import React from 'react';
-import {MemoryRouter} from 'react-router-dom';
-import {Leaderboard} from './Leaderboard';
 
-describe('Testing render, Navbar', () => {
+jest.mock('node-fetch');
 
-    beforeEach(() => {
-        render(<MemoryRouter>
-            <Leaderboard/>
-        </MemoryRouter>)
-    });
+describe('Leaderboard component', () => {
+  beforeEach(() => {
+    const mockData = {
+      leaderboard: [
+        {
+          organization: 'IT-tjenester',
+          active_percentage: 50.1,
+          rank: 5
+        },
+        {
+          organization: 'Berg skole',
+          active_percentage: 7.6,
+          rank: 101
+        }
+      ]
+    };
+    global.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockData)
+      })
+    );
 
-    afterEach(() => {
-        cleanup();
-    });
+    render(
+      <MemoryRouter>
+        <RecoilRoot>
+          <Leaderboard />
+        </RecoilRoot>
+      </MemoryRouter>
+    );
+  });
 
-    it("renders leaderboard without crashing", () => {
-        expect(screen.getByTestId('leaderboard')).toBeInTheDocument();
-    });
+  test('displays the correct organization', async () => {
+    const org1 = await screen.findByText('IT-tjenester');
+    const org2 = await screen.findByText('Berg skole');
+    expect(org1).toBeInTheDocument();
+    expect(org2).toBeInTheDocument();
+  });
+
+  test('displays the correct percentage', async () => {
+    const org1Perc = await screen.findByText('50.1');
+    expect(org1Perc).toBeInTheDocument();
+    const org2Perc = await screen.findByText('7.6');
+    expect(org2Perc).toBeInTheDocument();
+  });
+
+  test('displays the correct ranking', async () => {
+    const org1Rank = await screen.findByText('5');
+    expect(org1Rank).toBeInTheDocument();
+    const org2Rank = await screen.findByText('101');
+    expect(org2Rank).toBeInTheDocument();
+  });
+
+  test('displays the correct header', async () => {
+    expect(await screen.findByText('Topplisten')).toBeInTheDocument();
+  });
 });
